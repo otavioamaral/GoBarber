f9104eec23add5ad2b2efd331b24ce9c128b807e
otavioamaral <otavio.amaral@digitalpages.com.br>
2019-01-04 16:00:55 -0200

    GoBarber terminado

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '0.0.0.0',
+  port: '5433',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
